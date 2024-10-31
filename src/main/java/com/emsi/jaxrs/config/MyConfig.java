2b8ac77100c3597e6d925bb9a57854a9e4e4582a
ke-saad <kellalisaad@gmail.com>
2024-10-31 23:35:02 +0100

    Initial commit

--- src/main/java/com/emsi/jaxrs/config/MyConfig.java
+++ src/main/java/com/emsi/jaxrs/config/MyConfig.java
@@ -4,6 +4,8 @@ import org.glassfish.jersey.jackson.internal.jackson.jaxrs.json.JacksonJaxbJsonP
 import org.glassfish.jersey.server.ResourceConfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 
 @Configuration
 public class MyConfig {
@@ -11,8 +13,18 @@ public class MyConfig {
     @Bean
     public ResourceConfig resourceConfig() {
         ResourceConfig jerseyServlet = new ResourceConfig();
-        jerseyServlet.register(JacksonJaxbJsonProvider.class);
+        jerseyServlet.register(JacksonJaxbJsonProvider.class); // Uncomment this if needed
         jerseyServlet.register(com.emsi.jaxrs.rest.CompteRestJaxRSAPI.class);
         return jerseyServlet;
+    }
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        return new ObjectMapper(); // Configure as needed
+    }
+
+    @Bean
+    public XmlMapper xmlMapper() {
+        return new XmlMapper(); // Configure as needed
     }
 }
